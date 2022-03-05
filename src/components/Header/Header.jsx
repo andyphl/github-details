994819bd4107d3052ea72034dd18f798ea5e1960
andyphl <phlai951016@gmail.com>
2022-03-05 20:32:04 +0800

    Typo fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import styles from "./Header.module.scss";
 export const Header = () => {
   return (
     <header className={styles.header}>
-      <h1>Github Viewer</h1>
+      <h1>Github Details</h1>
     </header>
   );
 };
