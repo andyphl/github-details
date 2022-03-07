1bad26cb96cd7e7e8c2a6e91b9b99c4ba549278c
andyphl <phlai951016@gmail.com>
2022-03-07 21:44:49 +0800

    Add nav active style when pathname match

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,24 +1,25 @@
 import styles from "./Nav.module.scss";
 import { Link } from "react-router-dom";
-
+import { useLocation } from "react-router-dom";
 export const Nav = ({ active }) => {
+  const { pathname } = useLocation();
   return (
     <nav
       role="navigation"
       aria-label="Main menu"
       className={`${styles.nav} ${active ? styles.active : ""}`}
     >
-      <li>
+      <li className={pathname === "/" ? styles.active : ""}>
         <Link to="/" exact="true" className={styles.link}>
           Home
         </Link>
       </li>
-      <li className={styles.active}>
+      <li className={pathname === "/repos" ? styles.active : ""}>
         <Link to="/repos" className={styles.link}>
           Repositories
         </Link>
       </li>
-      <li>
+      <li className={pathname === "/followers" ? styles.active : ""}>
         <Link to="/followers" className={styles.link}>
           Followers
         </Link>
